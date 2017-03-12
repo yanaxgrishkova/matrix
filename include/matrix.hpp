8b967bd50f41df27cbb7fe1f6e54ab51ebf420bf
kate-lozovaya <katekalozovaya@gmail.com>
2017-03-12 17:19:55 +0300

    Update matrix.hpp

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -15,8 +15,10 @@ public:
 	~Matrix();
 	void scan(string filename)const;
 	void print(ostream&steam)const;
-	Matrix& operator= (const Matrix &C);
-	Matrix operator+ (Matrix B)const;
+	Matrix& operator = (const Matrix &C);
+	Matrix operator + (Matrix B)const;
 	Matrix operator * (Matrix B)const;
-	const bool operator==(const Matrix &C);
+	const bool operator == (const Matrix &C);
+	friend ostream& operator << (ostream &out, Matrix &C);
+	friend istream& operator >> (istream &in, Matrix &C);
 };
