72a5c7c3ee279706dc6d4dfc96d0ad8f0eb43777
kate-lozovaya <katekalozovaya@gmail.com>
2017-03-18 21:48:36 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -4,15 +4,15 @@
 SCENARIO("matrix init", "[init]") 
 {
 	Matrix matrix;
-	REQUIRE(matrix.rows() == 0);
-	REQUIRE(matrix.columns() == 0);
+	REQUIRE(matrix.line() == 0);
+	REQUIRE(matrix.column() == 0);
 }
 
 SCENARIO("matrix with params", "[init]") {
 	Matrix matrix(2,3);
-	REQUIRE(matrix.rows() == 2);
-	REQUIRE(matrix.columns() == 3);
-	for (int i = 0; i < matrix.rows(); ++i)
-		for (int j = 0; j < matrix.columns(); ++j)
+	REQUIRE(matrix.line() == 2);
+	REQUIRE(matrix.column() == 3);
+	for (int i = 0; i < matrix.line(); ++i)
+		for (int j = 0; j < matrix.column(); ++j)
 			REQUIRE(matrix[i][j] == 0;
 }
