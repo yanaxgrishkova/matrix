023c2d2d1b7d449b6c4ebd027cb13215d6dd247b
kate-lozovaya <katekalozovaya@gmail.com>
2017-03-18 21:17:53 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -12,11 +12,4 @@ SCENARIO("matrix with params", "[init]") {
 	matrix_t matrix(2,3);
 	REQUIRE(matrix.rows() == 2);
 	REQUIRE(matrix.columns() == 3);
-	for (unsigned int i = 0; i < matrix.rows(); ++i) 
-	{
-		for (unsigned int j = 0; j < matrix.rows(); ++j)
-		{
-			
-			REQUIRE(matrix[i][j] == 0);
-	}
 }
