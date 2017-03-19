17e17a3079ca6429775136632e46041686b9ef4c
kate-lozovaya <katekalozovaya@gmail.com>
2017-03-19 17:43:50 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -4,15 +4,50 @@
 SCENARIO("matrix init", "[init]") 
 {
 	Matrix matrix;
-	REQUIRE(matrix.line() == 0);
-	REQUIRE(matrix.column() == 0);
+	REQUIRE(matrix.line_() == 0);
+	REQUIRE(matrix.column_() == 0);
 }
 
-SCENARIO("matrix with params", "[init]") {
+SCENARIO("matrix with params", "[init]")
+{
 	Matrix matrix(2,3);
-	REQUIRE(matrix.line() == 2);
-	REQUIRE(matrix.column() == 3);
-	for (int i = 0; i < matrix.line(); ++i)
-		for (int j = 0; j < matrix.column(); ++j)
+	REQUIRE(matrix.line_() == 2);
+	REQUIRE(matrix.column_() == 3);
+	for (int i = 0; i < matrix.line_(); ++i)
+		for (int j = 0; j < matrix.column_(); ++j)
 			REQUIRE(matrix[i][j] == 0;
 }
+				
+SCENARIO("+", "[init]")
+{
+	Matrix A(2, 3);
+	Matrix B(2, 3);
+	Matrix C(2, 3);
+	for (int i = 0; i < 2; ++i)
+		for (int j = 0; j < 3; ++j)
+		{
+			A[i][j] = B[i][j] = 2;
+			C[i][j] = 4;
+		}
+	REQUIRE((A + B) == C);
+}
+
+SCENARIO("*", "[init]")
+{
+	Matrix A(2, 3);
+	Matrix B(3, 2);
+	Matrix C(2, 2);
+	for (int i = 0; i < 2; ++i)
+		for (int j = 0; j < 3; ++j)
+			A[i][j]  = 2;
+	for (int i = 0; i < 3; ++i)
+		for (int j = 0; j < 2; ++j)
+			B[i][j] = 3;
+	for (int i = 0; i < 2; ++i)
+		for (int j = 0; j < 2; ++j)
+			C[i][j] = 24;
+	REQUIRE((A * B) == C);
+}
+				
+
+
