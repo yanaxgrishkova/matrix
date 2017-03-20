67b36d616f99c6344fbf52b02d155f61f985e7f9
yanaxgrishkova <yanaxgrishkova@gmail.com>
2017-03-20 15:23:34 +0400

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -20,3 +20,13 @@ SCENARIO("copy constructor") {
 	REQUIRE(copy.cols_() == 3);
 }
 
+
+SCENARIO("copy constructor") {
+	Matrix m1 (3, 3);
+	Matrix m2 (3, 3);
+	Matrix m3 (3, 3);	
+	std::ifstream("m1.txt") >> m1;
+	std::ifstream("m2.txt") >> m2;
+	std::ifstream("m3.txt") >> m3;
+	REQUIRE(m1 + m2 == m3);
+}
