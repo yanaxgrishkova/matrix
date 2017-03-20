e5b2cc6122cedd8bec50af4ccb17563177c6406a
yanaxgrishkova <yanaxgrishkova@gmail.com>
2017-03-20 13:32:11 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -6,3 +6,9 @@ SCENARIO("default constructor") {
 	REQUIRE(matrix.rows_() == 0);
 	REQUIRE(matrix.cols_() == 0);
 }
+
+SCENARIO("constructor with params") {
+	Matrix matrix(3, 3);
+	REQUIRE(matrix.rows_() == 3);
+	REQUIRE(matrix.cols_() == 3);
+}
